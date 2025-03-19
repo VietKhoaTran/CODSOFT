f74636428257c1b7340ebb65c55f1460ecf183bf
VietKhoaTran <155411838+VietKhoaTran@users.noreply.github.com>
2025-03-19 10:07:33 +0800

    Add files via upload

--- EDA.py
+++ EDA.py
@@ -1,27 +1,24 @@
 import pandas as pd
- 
-df_train = pd.read_csv('CREDIT CARD FRAUD DETECTION\\Data\\fraudTrain.csv')
-df_test = pd.read_csv('CREDIT CARD FRAUD DETECTION\\Data\\fraudTest.csv')
+import numpy as np
 
-#understand the dataset
-print(df_train.info())
+df_train = pd.read_csv('MOVIE GENRE CLASSIFICATION\\Data\\train_data.txt', sep = ":::", names = ['ID', 'TITLE', 'GENRE', 'DESCRIPTION'], engine = 'python')
+df_test = pd.read_csv('MOVIE GENRE CLASSIFICATION\\Data\\test_data_solution.txt', sep = ":::", names = ['ID', 'TITLE','GENRE', 'DESCRIPTION'], engine = 'python')
+
+#Understanding the dataset
 print(df_train.head())
 
+print(df_train.info())
+print(df_test.info())
+
+#Visualizing distribution of the dataset
 import matplotlib.pyplot as plt
-# Distribution of fraud transaction across categories
-df_train['category'][df_train['is_fraud'] == 1].value_counts().plot(kind = 'bar')
+
+df_train['GENRE'].value_counts().plot(kind = 'bar')
 plt.xticks(rotation = 45)
-plt.title('Distribution of Fraud among category')
+plt.title('Distribution of Movie Genres for train dataset')
 plt.show()
 
-# Distribution of fraud transaction across numerical variables
-numerical_cols = df_train.select_dtypes(include=['float64', 'int64']).columns.to_list()
-remove = ['is_fraud', 'cc_num', 'Unnamed: 0']
-numerical_cols = list(filter(lambda x: x not in remove, numerical_cols))
-
-for i, col in enumerate(numerical_cols):
-    plt.subplot(3, 3, i+1)
-    plt.hist(df_train[col][df_train['is_fraud'] == 1], bins = 30)
-    plt.xlabel(f'{col}')
-    plt.ylabel('Frequency')
+df_test['GENRE'].value_counts().plot(kind = 'bar')
+plt.xticks(rotation = 45)
+plt.title('Distribution of Movie Genres for test dataset')
 plt.show()
--- main.py
+++ main.py
@@ -1,31 +1,60 @@
 import pandas as pd
- 
-df_train = pd.read_csv('CREDIT CARD FRAUD DETECTION\\Data\\fraudTrain.csv')
-df_test = pd.read_csv('CREDIT CARD FRAUD DETECTION\\Data\\fraudTest.csv')
+import numpy as np
 
-#EDA to show that this is imbalanced dataset, and make it balanced again
+df_train = pd.read_csv('MOVIE GENRE CLASSIFICATION\\Data\\train_data.txt', sep = ":::", names = ['ID', 'TITLE', 'GENRE', 'DESCRIPTION'], engine = 'python')
+x = df_train['DESCRIPTION'][:5000]
+y = df_train['GENRE'][:5000]
 
-x_train = df_train[['category', 'amt', 'zip', 'lat', 'long', 'city_pop', 'unix_time', 'merch_lat', 'merch_long']]
-y_train = df_train['is_fraud']
+# Preprocessing & Bag of words
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 
-x_test = df_test[['category', 'amt', 'zip', 'lat', 'long', 'city_pop', 'unix_time', 'merch_lat', 'merch_long']]
-y_test = df_test['is_fraud']
+ps = PorterStemmer()
+def preprocess_text(text):
+    text = re.sub('[^a-zA-Z]', ' ', str(text))
+    words = text.lower().split()
+    words = [ps.stem(word) for word in words if word not in stopwords.words('english')]
+    words = ' '.join(words)
+    return words
+x = [preprocess_text(doc) for doc in x]
 
-# Labelencoding x['fraud]
+# TFIDF vectorization
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+vectorizer = TfidfVectorizer(max_features = 5000)
+x = vectorizer.fit_transform(x)
+
+# Labelencoding y
 from sklearn.preprocessing import LabelEncoder
+
 encoder = LabelEncoder()
-x_train['category'], x_test['category'] = encoder.fit_transform(x_train['category']), encoder.fit_transform(x_test['category'])
+y = encoder.fit_transform(y)
 
+# Predict genre
+def predict_genre (description, model):
+    des = preprocess_text(description)
+    des = vectorizer.transform([des])
+    predicted_genre = model.predict(des)
+    genre_name = encoder.inverse_transform(predicted_genre)[0]
+    return genre_name
+
+# Model
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from xgboost import XGBClassifier
-from sklearn.metrics import accuracy_score
-models = [LogisticRegression(), DecisionTreeClassifier(), XGBClassifier()]
+from sklearn.svm import SVC
 
-for i in models:
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=42)
+
+list = [LogisticRegression(), MultinomialNB(), SVC()]
+description = 'His father has died, he hasnt spoken with his brother for about 10 years and has a serious cancer. Diego is a talented film director with difficulty to deal with his sickness, which is making him lose his friends and family. His best friend and doctor Ricardo gives him the news that he needs a bone marrow transplantation, otherwise hell die. He gets married to a beautiful woman, Livia, just before going to Seattle to get treatment. There, he undergoes numerous medical procedures. During treatment, he meets an Hindu boy, with whom he plays and whom he tells amazing stories. Odds are against him and when stakes are the highest, Diego gets a visit from a very uncommon man.'
+for i in list:
     i.fit(x_train, y_train)
     y_pred = i.predict(x_test)
     accuracy = accuracy_score(y_test, y_pred)
-    print(f'Accuracy for using model {i} is {accuracy*100: .2f}%')
-
+    predicted_genre = predict_genre(description, i)
+    print(f"Predicted Genre for above description using {i} is: {predicted_genre}")
+    print(f"Accuracy for {i}: {accuracy: .2f}")
